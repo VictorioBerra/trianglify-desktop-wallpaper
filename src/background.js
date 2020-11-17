7bb32428ada051c2dfb84df4f5e28bcaf79348a6
Victorio Berra <toryberra@gmail.com>
2020-11-16 20:26:01 -0600

    checkpoint

--- src/background.js
+++ src/background.js
@@ -1,189 +1,307 @@
-'use strict'
+"use strict";
 
-import path from 'path'
-import fs from 'fs'
-import wallpaper from 'wallpaper'
-import { app, protocol, BrowserWindow, screen, Tray, Menu, ipcMain } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import { v4 as uuidv4 } from 'uuid';
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-import mkdirp from 'mkdirp'
+import path from "path";
+import fs from "fs";
+import wallpaper from "wallpaper";
+import {
+  app,
+  protocol,
+  BrowserWindow,
+  screen,
+  Tray,
+  Menu,
+  ipcMain,
+} from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import { v4 as uuidv4 } from "uuid";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import mkdirp from "mkdirp";
+import settings from 'electron-settings';
+import { combinedDisposable } from "custom-electron-titlebar/lib/common/lifecycle";
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
-const iconpath = path.join(__static, 'icon.png')
+// Set up settings and defaults
+let imageSavePath = settings.getSync("image.folder");
+if (!imageSavePath) {
+  const defaultImageSavePath = path.join(
+    app.getPath("pictures"),
+    "trianglify"
+  );
+  mkdirp.sync(defaultImageSavePath);
+  imageSavePath = defaultImageSavePath;
+  settings.setSync("image.folder", defaultImageSavePath);
+}
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+const iconpath = path.join(__static, "icon.png");
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+  { scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
-app.setAboutPanelOptions({ 
-  applicationName: 'About Trianglify Wallpaper', 
-  applicationVersion: '1.0.0', 
-  copyright: 'Victorio Berra 2020',
-  authors: ['Victorio Berra'], 
-  website: 'https://www.tberra.com/', 
-  iconPath: iconpath, 
-})
+app.setAboutPanelOptions({
+  applicationName: "About Trianglify Wallpaper",
+  applicationVersion: "1.0.0",
+  copyright: "Victorio Berra 2020",
+  authors: ["Victorio Berra"],
+  website: "https://www.tberra.com/",
+  iconPath: iconpath,
+});
 
-let win = null
-let tray = null
+let win = null;
+let winPreferences = null;
+let tray = null;
 
 async function createWindow() {
   // Create the browser window.
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-  win = new BrowserWindow({
-    width: width * .75,
-    height: height * .75,
+  let mainWindow = new BrowserWindow({
+    width: width * 0.75,
+    height: height * 0.75,
     frame: false,
-    titleBarStyle: 'hidden',
-    title: 'Trianglify Wallpaper',
+    titleBarStyle: "hidden",
+    title: "Trianglify Wallpaper",
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js"),
     },
-    icon: iconpath
-  })
-
-  win.on('close', function (event) {
-      event.preventDefault();
-      win.hide();
+    icon: iconpath,
   });
 
-  tray = new Tray(iconpath)
+  mainWindow.on("close", function(event) {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  tray = new Tray(iconpath);
   const contextMenu = Menu.buildFromTemplate([
-    { 
-      label: 'Settings', 
-      type: 'normal', 
+    {
+      label: "Open",
+      type: "normal",
       click: function() {
-        win.show();
-      } 
+        mainWindow.show();
+      },
     },
-    { label: 'About', type: 'normal', role: "about", },
-    { 
-      label: 'Quit', 
-      type: 'normal', 
+    {
+      label: "Preferences",
+      type: "normal",
       click: function() {
-          win.destroy();
-          app.quit();
-      } 
+        winPreferences.show();
+      },
     },
-  ])
-  tray.setToolTip('Trianglify Wallpaper')
-  tray.setContextMenu(contextMenu)
+    { label: "About", type: "normal", role: "about" },
+    {
+      label: "Quit",
+      type: "normal",
+      click: function() {
+        mainWindow.destroy();
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Trianglify Wallpaper");
+  tray.setContextMenu(contextMenu);
 
   var menu = Menu.buildFromTemplate([
     {
-      label: 'File',
+      label: "File",
       submenu: [
         {
-          label:'Minimize', 
+          label: "Minimize",
           click: () => {
-            win.hide();
-          }
+            mainWindow.hide();
+          },
         },
-          {
-            label:'Quit', 
-            click: function() {
-              win.destroy();
-              app.quit();
-          } 
-        }
-      ]
-    }
-  ])
+        {
+          label: "Quit",
+          click: function() {
+            mainWindow.destroy();
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        {
+          label: "Preferences",
+          click: () => {
+            winPreferences.show();
+          },
+        },
+      ],
+    },
+  ]);
   Menu.setApplicationMenu(menu);
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    await mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    if (!process.env.IS_TEST) mainWindow.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    mainWindow.loadURL("app://./index.html");
   }
+
+  return mainWindow;
 }
 
-ipcMain.on('set-wallpaper-message', (event, dataUrl) => {
+async function createPreferencesWindow() {
 
-  const tempPath = path.join(app.getPath('temp'), 'images')
+  let preferencesWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    show: false,
+    titleBarStyle: "hidden",
+    title: "Trianglify Wallpaper Preferences",
+    webPreferences: {
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      preload: path.join(__dirname, "preloadPreferences.js"),
+    },
+    icon: iconpath,
+  });
 
-  // TODO: User settings like image save path...
-  mkdirp.sync(tempPath)
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    await preferencesWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL + "preferences");
+    if (!process.env.IS_TEST) preferencesWindow.webContents.openDevTools();
+  } else {
+    createProtocol("app");
+    // Load the index.html when not in development
+    preferencesWindow.loadURL("app://./preferences.html");
+  }
 
-  var newFileName = path.join(tempPath, `${uuidv4().toString()}.png`);
-
-  console.log("newFileName", newFileName);
-  console.log("tempPath", tempPath);
-  //console.log("dataUrl", dataUrl);
-
-  // dataUrl to buffer
-  var buffer = Buffer.from(dataUrl.split(",")[1], 'base64');
-  fs.writeFileSync(newFileName, buffer);
-
-  wallpaper.set(newFileName);
-
-  event.reply('set-wallpaper-reply', 'done')
-})
-
-// Sync
-ipcMain.on('get-path-message', (event, arg) => {
-  event.returnValue = app.getPath(arg)
-})
-
-// Sync
-ipcMain.on('get-screens-message', (event, arg) => {
-  event.returnValue = {
-    all: screen.getAllDisplays(),
-    primary: screen.getPrimaryDisplay()
-  };
-})
+  return preferencesWindow;
+}
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", async () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/recipes.html#multiple-pages
+  if (win === null) {
+    win = await createWindow('', 'index.html')
+  }
+  if (winPreferences === null) {
+    winPreferences = await createPreferencesWindow('preferences', 'preferences.html')
+  }
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow()
-})
+  win = await createWindow();
+  winPreferences = await createPreferencesWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
+
+
+
+
+
+
+
+
+// TODO redo all this this way https://jaketrent.com/post/select-directory-in-electron/
+
+ipcMain.on("set-wallpaper-message", async (event, dataUrl) => {
+
+  let userSettings = await settings.get();
+
+  let newFileName = path.join(userSettings.image.folder, `${uuidv4().toString()}.png`);
+
+  // dataUrl to buffer
+  let buffer = Buffer.from(dataUrl.split(",")[1], "base64");
+
+  try
+  {
+    fs.writeFileSync(newFileName, buffer);
+  }
+  catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+    event.reply("set-wallpaper-reply", `No such file or directory '${userSettings.image.folder}'`);
+    return;
+  }
+
+  wallpaper.set(newFileName);
+
+  event.reply("set-wallpaper-reply");
+});
+
+ipcMain.on('set-window-hide-message', (event, arg) => {
+  console.log('set-window-hide-message', arg);
+  if(arg === "main") {
+    win.hide();
+  }
+  if(arg === "preferences") {
+    winPreferences.hide();
+  }
+});
+
+ipcMain.on('set-window-show-message', (event, arg) => {
+  if(arg === "main") {
+    winPreferences.show();
+  }
+  if(arg === "preferences") {
+    winPreferences.show();
+  }
+});
+
+// Sync
+ipcMain.on("get-path-message", (event, arg) => {
+  event.returnValue = app.getPath(arg);
+});
+
+// Sync
+ipcMain.on("get-screens-message", (event, arg) => {
+  event.returnValue = {
+    all: screen.getAllDisplays(),
+    primary: screen.getPrimaryDisplay(),
+  };
+});
+
+ipcMain.on("set-preferences-closed", (event, arg) => {
+  event.returnValue = {
+    all: screen.getAllDisplays(),
+    primary: screen.getPrimaryDisplay(),
+  };
+});
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <v-row class="mb-6">
+  <v-container fluid>
+    <v-row class="mb-6" justify="center">
       <!-- <v-col cols="12">
         <v-img
           :src="require('../assets/logo.svg')"
@@ -15,11 +15,13 @@
         <v-list dense>
           <v-subheader>Screens</v-subheader>
           <v-list-item-group
+            v-model="selectedScreenId"
             color="primary"
           >
             <v-list-item
-              v-for="(screen, i) in screens"
-              :key="i"
+              v-for="screen in screens"
+              :key="screen.id"
+              :value="screen.id"
             >
               <v-list-item-content>
                 <v-list-item-title v-show="screen.isPrimary" class="text-uppercase">primary display</v-list-item-title>
@@ -28,15 +30,58 @@
             </v-list-item>
           </v-list-item-group>
         </v-list>
+
+      <v-expansion-panels accordion v-model="customizationPanel">
+        <v-expansion-panel>
+          <v-expansion-panel-header ripple>Customize</v-expansion-panel-header>
+          <v-expansion-panel-content>
+
+          <v-row>
+            <v-col cols="4">
+              <v-text-field
+                v-model="selectedScreenHeight"
+                label="Height"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="selectedScreenWidth"
+                label="Width"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+            <v-slider
+              label="Triangle Variance"
+              v-model="triangleVariance"
+              max="1"
+              min="0"
+              step="0.01"
+            ></v-slider>
+
+            <v-slider
+              label="Cell Size"
+              v-model="cellSize"
+              max=".25"
+              min=".02"
+              step="0.01"
+            ></v-slider>
+
+
+
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
       </v-col>
 
       <v-col
         cols="8"
       >
 
-        <v-img v-bind:src="wallpaper" />
+        <canvas id="c" height="900" width="1440" />
         
-        <v-btn v-on:click="generate"
+        <v-btn v-on:click="generateTrianglifyCanvas"
           class="mt-6"
           elevation="2"
           color="primary"
@@ -56,18 +101,76 @@
     </v-row>
   </v-container>
 </template>
-
+<style scoped>
+  #c {
+    max-height: 100%;
+    max-width: 100%;
+    flex: 1 1 auto;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+    filter: drop-shadow(2px 2px 8px rgba(0,0,0,.2));
+  }
+</style>
 <script>
 import trianglify from 'trianglify'
-// import { v4 as uuidv4 } from "uuid"
+import _ from 'lodash'
 
 export default {
   data: () => ({
+    customizationPanel: 0,
     screens: [],
-    wallpaper: null
+    wallpaper: null,
+
+    selectedScreenId: null,
+    selectedScreen: null,
+    selectedScreenHeight: null,
+    selectedScreenWidth: null,
+
+    triangleVariance: 0.21,
+    cellSize: 0.05,
+
   }),
+  watch: {
+    selectedScreenId: function (val) {
+      let selectedScreen = this.screens.find(x => x.id == val);
+      this.selectedScreen = selectedScreen;
+      this.selectedScreenHeight = selectedScreen.size.height;
+      this.selectedScreenWidth = selectedScreen.size.width;
+    },
+    selectedScreenWidth: function(newValue, oldValue) {
+        this.vueCanvas.clearRect(0, 0, oldValue, oldValue);
+        this.generateTrianglifyCanvas()
+    },
+    selectedScreenHeight: function(newValue, oldValue) {
+        this.vueCanvas.clearRect(0, 0, oldValue, oldValue);
+        this.generateTrianglifyCanvas()
+    },
+    triangleVariance: function() {
+        this.generateTrianglifyCanvas()
+    },
+    cellSize: function() {
+        this.generateTrianglifyCanvas()
+    },
+  },
+  // computed: {
+  //   selectedScreen: function () {
+  //    
+  //   }
+  // },
   name: 'HelloWorld',
-  created: function(){
+  mounted (){
+    window.ipcRenderer.on('set-wallpaper-reply', this.wallpaperSetEventHandler)
+    // How to unmount this?
+    var c = document.getElementById("c")
+    var ctx = c.getContext("2d")
+    this.vueCanvas = c;
+    this.vueCanvasContext = ctx;
+  },
+  destroyed () {
+    window.ipcRenderer.removeListener('set-wallpaper-reply', this.wallpaperSetEventHandler)
+  },
+  created: function() {
 
     this.tempPath = window.ipcRenderer.sendSync('get-path-message', 'temp');
     const screens = window.ipcRenderer.sendSync('get-screens-message');
@@ -80,7 +183,28 @@ export default {
       };
     });
 
-    this.generate();
+    let selectedScreen = this.screens.find(x => x.isPrimary);
+    this.selectedScreenId = selectedScreen.id;
+    
+    // https://stackoverflow.com/a/49780382/1777780
+    this.generateTrianglifyCanvas = _.debounce(() => {
+
+            let opts = {
+              width: this.selectedScreenWidth,
+              height: this.selectedScreenHeight,
+              cellSize: Math.max(this.selectedScreenWidth, this.selectedScreenHeight) * this.cellSize,
+              variance: this.triangleVariance
+            }
+
+            const pattern = trianglify(opts);
+            const canvas = pattern.toCanvas(this.vueCanvas, {
+              applyCssScaling: false
+            })
+            
+            this.wallpaper = canvas.toDataURL()
+      }, 300)
+
+    this.generateTrianglifyCanvas();
 
   },
   methods: {
@@ -93,27 +217,7 @@ export default {
     },
     save: async function()
     {
-        // const pattern = trianglify({
-        //   width: 1024,
-        //   height: 1024
-        // });
-
-        // const canvas = pattern.toCanvas();
-
-        // // See the node-canvas docs for a full
-        // // list of all the things you can do with this Canvas object:
-        // // https://github.com/Automattic/node-canvas
-        // var newWallpaperDataUrl = canvas.toDataURL();
-
-        // TODO: Dont expose ipcRenderer, just expose callbacks.
-        window.ipcRenderer.on('set-wallpaper-reply', () => {
-          // some UI thing?
-        })
         window.ipcRenderer.send('set-wallpaper-message', this.wallpaper)
-
-
-        //         const fileName = path.join(this.tempPath, 'images', `${uuidv4().toString()}.png`)
-        // const file = fs.createWriteStream(fileName)
     }
   }
 }
